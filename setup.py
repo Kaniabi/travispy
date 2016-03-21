a515469ff9e87b14c6af6b9699c265ccbda093d7
Alexandre Andrade <kaniabi@gmail.com>
2016-03-21 10:54:18 -0300

    Using pytest-runner on setup.py.

--- setup.py
+++ setup.py
@@ -3,28 +3,6 @@ from setuptools.command.test import test as TestCommand
 import sys
 
 
-class PyTest(TestCommand):
-
-    user_options = [('pytest-args=', 'a', 'Arguments to pass to py.test')]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        import pytest
-        args = ['travispy']
-        if self.pytest_args:
-            args.insert(0, self.pytest_args)
-        errno = pytest.main(args)
-        sys.exit(errno)
-
-
 setup(
     name='TravisPy',
     version='0.3.4',
@@ -50,7 +28,6 @@ setup(
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
 
-    # tests
+    setup_requires=['pytest-runner'],
     tests_require=['pytest'],
-    cmdclass={'test': PyTest},
 )
